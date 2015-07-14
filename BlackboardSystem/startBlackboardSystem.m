e9f854070ee420b27f1640a0235826d65d70892b
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-07-14 11:45:31 +0200

    Add src/ folder to BlackboardSystem

--- BlackboardSystem/startBlackboardSystem.m
+++ BlackboardSystem/startBlackboardSystem.m
@@ -1,14 +1,14 @@
 % This script initialises the path variables that are needed for running
 % the Two!Ears Blackboard System module
 
-basePath = [fileparts(mfilename('fullpath')) filesep];
+basePath = fileparts(mfilename('fullpath'));
 
 % Add all relevant folders to the matlab search path
-addpath([basePath 'blackboard_core']);
-addpath([basePath 'blackboard_data']);
-addpath([basePath 'evaluation']);
-addpath([basePath 'gmtk_matlab_interface']);
-addpath([basePath 'identificationTraining']);
-addpath([basePath 'knowledge_sources']);
+addpath(fullfile(basePath, 'src', 'blackboard_core'));
+addpath(fullfile(basePath, 'src', 'blackboard_data'));
+addpath(fullfile(basePath, 'src', 'evaluation'));
+addpath(fullfile(basePath, 'src', 'gmtk_matlab_interface'));
+addpath(fullfile(basePath, 'src', 'identificationTraining'));
+addpath(fullfile(basePath, 'src', 'knowledge_sources'));
 
 clear basePath;
