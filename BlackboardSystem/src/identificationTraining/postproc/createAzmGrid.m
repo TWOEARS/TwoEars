97dd331b86ef9e1d0fdeb9e7591fe7c3b65d9594
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-08-07 09:45:08 +0200

    Remove IdentificationTraining from Blackboard

--- BlackboardSystem/src/identificationTraining/postproc/createAzmGrid.m
+++
@@ -1,4 +0,0 @@
-function azmGrid = createAzmGrid( className, dispLatexCode )
-
-azms = [0, 45, 90, 135, 180];
-azmGrid = createTrTeGrid( 'trainings/azmTrain', className, azms, @azmCmpCvAndTestPerf, dispLatexCode, '\textdegree' );
