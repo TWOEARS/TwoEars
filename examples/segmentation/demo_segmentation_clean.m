707c005090b328becf59acacd1edc5436047fafb
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-12-16 17:31:27 +0100

    Add missing segmentation example

---
+++ examples/segmentation/demo_segmentation_clean.m
@@ -0,0 +1,63 @@
+%% Demo 1: Segmentation of an acoustic mixture without background noise
+%
+% This script demonstrates the usage of the Segmentation Knowledge Source
+% with the blackboard system for a mixture of 3 sound sources without
+% additional background noise in anechoic conditions. The acoustic scene 
+% contains three speakers at 30°, 0° and -30°.
+%
+% AUTHOR:
+%   Christopher Schymura (christopher.schymura@rub.de)
+%   Cognitive Signal Processing Group
+%   Ruhr-Universitaet Bochum
+%   Universitaetsstr. 150, 44801 Bochum
+
+% Initialize Binaural Simulator
+sim = simulator.SimulatorConvexRoom('test_scene_clean.xml');
+
+% Suppress simulator messages
+set(sim, 'Verbose', false);
+
+% Set look direction to zero degrees
+sim.rotateHead(0, 'absolute');
+
+% Initialize simulation
+set(sim, 'Init', true);
+
+% Initialize Blackboard System
+bbs = BlackboardSystem(false);
+bbs.setRobotConnect(sim);
+bbs.buildFromXml('segmentation_blackboard_clean.xml');
+
+% Start Blackboard System and run simulation
+bbs.run();
+
+% Shut down simulation
+set(sim, 'ShutDown', true);
+
+% Get segmentation results and plot them
+hypotheses = bbs.blackboard.data.values;
+
+figure(1)
+for k = 1 : 3
+    % Get hypotheses for current source
+    softMask = hypotheses{end}.segmentationHypotheses(k).softMask;
+    position = hypotheses{end}.positionHypotheses(k).sourcePosition;
+    
+    % Convert position from [rad] to [deg]
+    position = position * 180 / pi;
+    
+    % Get number of frames/channels and compute time and frequency-scales
+    [nFrames, nChannels] = size(softMask);
+    timescale = linspace(0, sim.LengthOfSimulation, nFrames);
+    freqscale = 1 : nChannels;
+    
+    subplot (1, 3, k)
+    imagesc(timescale, freqscale, softMask');
+    set(gca, 'YDir', 'normal');
+    axis square;
+    colorbar;
+    caxis([0, 1]);
+    xlabel('Time / [s]');
+    ylabel('Channel index');
+    title(['Estimated soft mask for source at ', num2str(position), '°.'])
+end
---
+++ examples/segmentation/demo_train_segmentation.m
@@ -0,0 +1,57 @@
+% This script demonstrates how an instance of the Segmentation
+% Knowledge Source can be trained for a specific set of HRTFs. The KS
+% performs a segmentation of auditory features and estimates the angular
+% positions of an acoustic mixture, given the number of sound sources 
+% that are present.
+%
+% AUTHOR:
+%   Christopher Schymura (christopher.schymura@rub.de)
+%   Cognitive Signal Processing Group
+%   Ruhr-Universitaet Bochum
+%   Universitaetsstr. 150, 44801 Bochum
+
+%% Step 1: Create an instance of the KS that should be trained
+
+% Each instance needs an unique identifier, specified as a string
+ksName = 'DemoKS';
+
+% Additional parameters for pre-processing can be specified. Note that if
+% the parameters are changed, the KS has to be re-trained for the new
+% parameters. The specification of these parameters is optional. If no
+% parameters are set, training will be conducted using default settings,
+% which are specified in the corresponding KS class.
+nChannels = 32;             % Number of filterbank channels
+winSize = 0.02;             % Size of the processing window in [s]
+hopSize = 0.01;             % Frame shift in [s]
+fLow = 80;                  % Lowest filterbank center frequency in [Hz]
+fHigh = 8000;               % Highest filterbank center frequency in [Hz]
+
+% Create an instance of the KS
+segKS = SegmentationKS(ksName, ...
+    'NumChannels', nChannels, ...
+    'WindowSize', winSize, ...
+    'HopSize', hopSize, ...
+    'FLow', fLow, ...
+    'FHigh', fHigh, ...
+    'Verbosity', true);     % Enable status messages during training
+
+%% Step 2: Run training
+
+% Generate a training dataset for a training scene. The training scene has
+% to be specified as a XML-file which can be handled by the Binaural
+% Simulator and contains the HRTF dataset that should be used for training.
+xmlSceneDescription = 'training_scene.xml';
+segKS.generateTrainingData(xmlSceneDescription);
+
+% Train the model. This may take several hours depending on your machine.
+% If you already have conducted training for your KS and you want to
+% re-train your models, you have to set the training method in overwriting
+% mode, which is set to 'false' by default (see KS class file for details).
+segKS.train();
+
+% Use the following command if you want to overwrite an existing model:
+% segKS.train(true);
+
+% Remove the generated training dataset. This data is not necessary for
+% using the KS within the blackboard system.
+segKS.removeTrainingData();
