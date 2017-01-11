4732788ea3b381e5196a7aeb755018ba4b2e73cf
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-01-11 10:17:08 +0100

    Update QoE coloration example

--- examples/qoe_coloration/common/plotColoration.m
+++ examples/qoe_coloration/common/plotColoration.m
@@ -14,16 +14,18 @@ function plotColoration(predictions, sourceTypes, humanLabelFiles, conditions, c
 %       colors          - cell with Matlab color names
 
 figure
-hold on
+hold on;
 for ii = 1:size(humanLabelFiles,1)
     humanLabels = readHumanLabels(humanLabelFiles{ii});
     errorbar(([humanLabels{:,2}]+1)./2, [humanLabels{:,3}]./2, ['o', colors{ii}]);
-    plot(predictions(ii,:), ['-', colors{ii}]);
+    p(ii) = plot(predictions(ii,:), ['-', colors{ii}]);
 end
+hold off;
 axis([0 11 0 1]);
 xlabel('System');
 ylabel('Coloration');
 set(gca, 'XTick', [1:10]);
 set(gca, 'XTickLabel', conditions);
+legend(p, sourceTypes, 'location', 'northwest');
 
 % vim: set sw=4 ts=4 et tw=90:
